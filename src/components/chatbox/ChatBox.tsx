c5d71ad117bab9e2a581ed64b916039914c0994b
Verastian <verastian0908@gmail.com>
2024-09-04 23:08:16 -0400

    refactor:changes in appearance

--- src/components/chatbox/ChatBox.tsx
+++ src/components/chatbox/ChatBox.tsx
@@ -4,6 +4,8 @@ import { MessageCircle, X } from 'lucide-react'
 import { ChatHeader, ChatInput, MemoizedEmojiPicker as EmojiPicker, MemoizedMessagesList as MessagesList, TypingIndicator } from '@/components'
 import { useChatStore } from '@/store'
 import { Props } from '@/interface'
+import Lottie from 'react-lottie-player'
+import chat2 from "../../../public/img/lottie/lottieflow-chat22-dark.json";
 
 export const ChatBox: React.FC<Props> = ({ isDarkMode }) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -16,7 +18,10 @@ export const ChatBox: React.FC<Props> = ({ isDarkMode }) => {
       initializeChat()
     }
   }, [isOpen, initializeChat])
-
+  const lottieStyles = {
+    width: 25,
+    height: 25,
+  };
   return (
     <div className="fixed bottom-4 right-4">
       <button
@@ -25,17 +30,27 @@ export const ChatBox: React.FC<Props> = ({ isDarkMode }) => {
         aria-expanded={isOpen}
         aria-haspopup="dialog"
       >
-        {isOpen ? <X className="w-6 h-6" /> : <MessageCircle className="w-6 h-6" />}
+        {isOpen ? <X className="w-6 h-6" /> :
+          // <MessageCircle className="w-6 h-6" />
+          <Lottie
+            loop={true}
+            play={!isOpen}
+            animationData={chat2}
+            // onLoopComplete={handleEndAnimated}
+            // onClick={scrollToTop}
+            // segments={[1, 300]}
+            style={lottieStyles}
+            speed={1}
+          />
+        }
       </button>
       {isOpen && (
         <div
-          className={`absolute bottom-16 right-0 w-[350px] h-[500px] flex flex-col shadow-2xl transition-all duration-300 ease-in-out animate-[slideIn_0.3s_ease-out] ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white'} rounded-lg overflow-hidden`}
+          className={`bg-imageBack_1 bg-cover absolute bottom-16 right-0 w-[350px] h-[500px] flex flex-col shadow-2xl 
+            transition-all duration-300 ease-in-out animate-[slideIn_0.3s_ease-out] 
+            ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white'} rounded-lg overflow-hidden`}
           role="dialog"
           aria-label={'chatAssistant'}
-          style={{
-            backgroundImage: isDarkMode ? 'none' : `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-            backgroundRepeat: 'repeat'
-          }}
         >
           <ChatHeader isDarkMode={isDarkMode} />
           <MessagesList messages={messages} isDarkMode={isDarkMode} />
--- src/components/ui/widgets/TypingIndicator.tsx
+++ src/components/ui/widgets/TypingIndicator.tsx
@@ -1,11 +1,28 @@
 'use client'
 import { TypingIndicatorProps } from '@/interface'
+import dynamic from 'next/dynamic';
+import typingDark from "../../../../public/img/lottie/lottieflow-loading-dark.json";
+import typingLight from "../../../../public/img/lottie/lottieflow-loading-light.json";
 import React from 'react'
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
 
 export const TypingIndicator: React.FC<TypingIndicatorProps> = ({ isDarkMode }) => {
+
+    const lottieStyles = {
+        width: 30,
+        height: 30,
+    };
+
     return (
-        <div className={`text-sm ${isDarkMode ? 'text-gray-400' : 'text-gray-500'} ml-2 mb-2`}>
+        <div className={`text-sm flex 
+        ${isDarkMode ? 'text-gray-400' : 'text-gray-800 font-medium'} ml-2 mb-2`}>
             {'typing'}
+            <Lottie
+                loop={true}
+                play={true}
+                animationData={isDarkMode ? typingDark : typingLight}
+                style={lottieStyles}
+            />
         </div>
     )
 }
